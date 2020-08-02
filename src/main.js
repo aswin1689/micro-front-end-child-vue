83f878f7c04569cce9b0257371d27bad95a2d8d2
aswin kumar alapati <aswinalapati1@gmail.com>
2020-08-02 08:06:56 -0400

    add container div

--- src/main.js
+++ src/main.js
@@ -4,14 +4,12 @@ import App from './App.vue';
 Vue.config.productionTip = false;
 
 window.renderVueApp = (containerId) => {
-	console.log('in render vue app')
 	new Vue({
 		render: (h) => h(App),
 	}).$mount(document.getElementById(containerId));
 };
 
 window.unmountVueApp = (containerId) => {
-	console.log('in vue unmount')
 	new Vue({
 		render: (h) => h(App),
 	}).$destroy(document.getElementById(containerId));
